7087c7743fb909950c1160aabb13453e6e03eb0c
Alexander <mrkennedyua@gmail.com>
2020-08-14 21:49:01 +0500

    placeholder result handling

--- app/src/main/java/dev/steelahhh/weatherapp/features/addresspicker/AddressPickerFragment.kt
+++ app/src/main/java/dev/steelahhh/weatherapp/features/addresspicker/AddressPickerFragment.kt
@@ -1,5 +1,7 @@
 package dev.steelahhh.weatherapp.features.addresspicker
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,4 +15,19 @@ import dev.steelahhh.weatherapp.R
 @AndroidEntryPoint
 class AddressPickerFragment : Fragment(R.layout.fragment_address_picker) {
     private val vm: AddressPickerViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        // TODO add this to some button that closes the picker
+        // setOnClickListener {
+        //     setFragmentResult(ADDRESS_RESULT_KEY, bundleOf(ADDRESS_RESULT_KEY to Any()))
+        //
+        //     findNavController().navigateUp()
+        // }
+    }
+
+    companion object {
+        const val ADDRESS_REQUEST_ID = "request:pick_address"
+
+        const val ADDRESS_RESULT_KEY = "key:address"
+    }
 }
--- app/src/main/java/dev/steelahhh/weatherapp/features/home/HomeFragment.kt
+++ app/src/main/java/dev/steelahhh/weatherapp/features/home/HomeFragment.kt
@@ -1,9 +1,13 @@
 package dev.steelahhh.weatherapp.features.home
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import dev.steelahhh.weatherapp.R
+import dev.steelahhh.weatherapp.features.addresspicker.AddressPickerFragment.Companion.ADDRESS_REQUEST_ID
 
 /*
  * Author: steelahhh
@@ -13,4 +17,12 @@ import dev.steelahhh.weatherapp.R
 @AndroidEntryPoint
 class HomeFragment : Fragment(R.layout.fragment_home) {
     private val vm: HomeViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setFragmentResultListener(ADDRESS_REQUEST_ID) { key, bundle ->
+            if (key == ADDRESS_REQUEST_ID) {
+                // TODO: handle the picked address
+            }
+        }
+    }
 }
